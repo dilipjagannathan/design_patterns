4e2b82ee3ee8ebc7b7ba4f21271e420efbd7cb4e
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2023-04-10 16:16:41 +0300

    Updated dependencies

---
+++ src/main/java/playground/PolymorphicBuilderDemo.java
@@ -0,0 +1,12 @@
+package playground;
+
+public class PolymorphicBuilderDemo {
+    public static void main(String... args) throws InterruptedException {
+        // Thread helloWorld = Thread.ofVirtual()
+        //     .name("myThread")
+        //     // .daemon()
+        //     // .priority(Thread.MAX_PRIORITY)
+        //     .start(() -> System.out.println("Hello world"));
+        // helloWorld.join();
+    }
+}
